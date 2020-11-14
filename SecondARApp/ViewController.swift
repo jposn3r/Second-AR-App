90f6a016c0dfbb89a62dd562d1ec00cff7e5f447
Jake <jposner91@gmail.com>
2020-11-14 13:30:59 -0500

    initial commit after fixing all xcode issues with first project

--- SecondARApp/ViewController.swift
+++ SecondARApp/ViewController.swift
@@ -15,10 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
     }
 }
